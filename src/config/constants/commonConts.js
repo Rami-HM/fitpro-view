72bc5ae535600980dc00824d64407e571b258def
RAMI-HM <anodaz7141@gmail.com>
2021-08-19 09:11:49 +0900

    build option 변경 및 server_url 변경

--- src/config/constants/commonConts.js
+++ src/config/constants/commonConts.js
@@ -1,5 +1,5 @@
- export const SERVER_URL = 'http://127.0.0.1';
-//export const SERVER_URL = 'https://fitpro-server.herokuapp.com';
+//export const SERVER_URL = 'http://127.0.0.1';
+export const SERVER_URL = 'https://fitpro-server.herokuapp.com';
 
 export const DELETE_TITLE ='삭제 하시겠습니까?';
 export const DELETE_CONTENTS = '삭제 후 복구가 불가능 합니다. \n정말 삭제 하시겠습니까?';
